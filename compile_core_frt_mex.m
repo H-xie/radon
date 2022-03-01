8b814367ec5f5eb045da628daabc20589694b1fc
Heaven <7456793+H-xie@users.noreply.github.com>
2022-03-01 09:45:41 +0800

    Fix path

--- compile_core_frt_mex.m
+++ compile_core_frt_mex.m
@@ -1,3 +1,3 @@
-str = ['mex CXXFLAGS="$CXXFLAGS -O4 -std=c++11" ' getenv('MAT') '/+radon/coreFRT.cpp -outdir ' getenv('MAT') '/+radon'];
+str = ['mex CXXFLAGS="$CXXFLAGS -O4 -std=c++11" ' getenv('MAT') './+radon/coreFRT.cpp -outdir ' getenv('MAT') './+radon'];
 
 eval(str);
